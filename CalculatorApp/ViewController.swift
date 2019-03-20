de36468406834ec984ef15203aaa8111daff2bc0
Yaroslav Stupinskyi <jaros.sv@gmail.com>
2019-03-20 08:26:12 +0200

    Initial Commit

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by Jaroslav Stupinskyi on 20.03.19.
+//  Copyright © 2019 Jaroslav Stupinskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
